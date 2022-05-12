77510397860840ec5a509521c71b2a8ffd57ff4c
rianders <rianders@docs.rutgers.edu>
2022-05-12 16:19:11 -0400

    Update face_mesh.py

--- face_mesh.py
+++ face_mesh.py
@@ -2,7 +2,9 @@ from cvzone.FaceMeshModule import FaceMeshDetector
 import cv2
 
 cap = cv2.VideoCapture(1)
-detector = FaceMeshDetector(maxFaces=2)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = FaceMeshDetector(maxFaces=1)
 while True:
     success, img = cap.read()
     img, faces = detector.findFaceMesh(img)
