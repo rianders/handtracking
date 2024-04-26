975bba7ed1dcfdbb02d625d5872306e4ddf04542
Rick Anderson <rianders@docs.rutgers.edu>
2024-04-26 19:21:25 -0400

    INcreased the screen size for better tracking

--- handtracking/Main.py
+++ handtracking/Main.py
@@ -3,40 +3,43 @@ import cv2
 import socket
 
 cap = cv2.VideoCapture(0)
-#cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
+
 success, img = cap.read()
 h, w, _ = img.shape
-
 detector = HandDetector(detectionCon=0.8, maxHands=2)
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddressPort = ("127.0.0.1", 5052)
 
+flip = False
+
 while True:
-    # Get image frame
     success, img = cap.read()
-    # Find the hand and its landmarks
-    hands, img = detector.findHands(img)  # with draw
-    # hands = detector.findHands(img, draw=False)  # without draw
+    hands, img = detector.findHands(img)
 
     data = []
     if hands:
-        # Hand 1
         hand1 = hands[0]
-        lmList1 = hand1["lmList"]  # List of 21 Landmark points
+        lmList1 = hand1["lmList"]
         for lm in lmList1:
             data.extend([lm[0], h - lm[1], lm[2]])
 
-        # Hand 2 (if detected)
         if len(hands) == 2:
             hand2 = hands[1]
-            lmList2 = hand2["lmList"]  # List of 21 Landmark points
+            lmList2 = hand2["lmList"]
             for lm in lmList2:
                 data.extend([lm[0], h - lm[1], lm[2]])
 
     sock.sendto(str.encode(str(data)), serverAddressPort)
 
-    # Display
+    if cv2.waitKey(1) == 27:  # 'Esc' key to exit
+        break
+
+    if cv2.waitKey(1) == ord('f'):
+        flip = not flip
+
+    if flip:
+        img = cv2.flip(img, 1)  # Flip horizontally
+
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
