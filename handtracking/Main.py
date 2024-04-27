a92fa3c3e30f1f356a1080a0f5e5cbf6023c7047
Rick Anderson <rianders@docs.rutgers.edu>
2024-04-27 15:25:16 -0400

    Rutgers Day updates

--- handtracking/Main.py
+++ handtracking/Main.py
@@ -2,7 +2,7 @@ from cvzone.HandTrackingModule import HandDetector
 import cv2
 import socket
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
 
